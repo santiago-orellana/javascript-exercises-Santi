abb30db21262bb1e1bd9b408d0b886df5e9d76e5
santiago-orellana <135621516+santiago-orellana@users.noreply.github.com>
2025-11-19 12:57:46 +0100

    Ex javascript

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    if (num < 0) {
+        return 'ERROR';
+    }
 
+    let stringRepeat = '';
+    let i = 0;
+    while (i < num) {
+        stringRepeat += string;
+        i++;
+    }
+    return stringRepeat;
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    
+    return word.split("").reverse().join("");
+    
+    // let reverseWord = '';
+    // for(let i = word.length - 1; i >= 0; i--) {
+    //     reverseWord += word[i];
+    // }
+    // return reverseWord;
 };
 
 // Do not edit below this line
