19e3e2017202559543b0df5e781d81ab1b078ae2
santiago-orellana <135621516+santiago-orellana@users.noreply.github.com>
2025-11-20 14:15:49 +0100

    Ex removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argument) {
+    
+    let newArray = array.filter(elemento => !argument.includes(elemento));
+    console.log("newArray: ", newArray);
+
+    return newArray;
 };
 
 // Do not edit below this line
