af5e024e49d3ed6b908888a403f74bac8b04e57e
santiago-orellana <135621516+santiago-orellana@users.noreply.github.com>
2025-11-21 13:41:29 +0100

    Ex 8 y 9

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,10 +1,23 @@
 const removeFromArray = function(array, ...argument) {
     
-    let newArray = array.filter(elemento => !argument.includes(elemento));
-    console.log("newArray: ", newArray);
+    let newArray = [];
+
+    array.forEach(item => {
+        if (!argument.includes(item)) {
+            newArray.push(item);
+        }
+    });
 
     return newArray;
+
+    // VERSION AVANZADA
+    // let newArray = array.filter(elemento => !argument.includes(elemento));
+    // console.log("newArray: ", newArray);
+
+    // return newArray;
 };
+
+// console.log(removeFromArray([2, 1, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a , b) { // a = 10; b = 2 
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    } else if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }else if (b < a) {
+        let x = b; // 2
+        b = a; // 10
+        a = x; // 2
+    }
 
+    let suma = 0;
+    for (let y = a; y <= b; y++) {
+        suma = suma + y;
+    }
+
+    console.log('Suma Total = ', suma);
+    return suma;
 };
 
 // Do not edit below this line
