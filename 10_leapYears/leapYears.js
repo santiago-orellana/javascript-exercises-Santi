e2a0ab36f12cbbcc57afc42126f469713931d384
santiago-orellana <135621516+santiago-orellana@users.noreply.github.com>
2025-11-24 13:06:58 +0100

    Ex10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,29 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 400 == 0) {
+        return true;
+    }
+    if (year % 100 == 0) {
+        return false;
+    }
+    if (year % 4 == 0) {
+        return true;
+    }
+    return false;
 
+    // const isYearDivisibleByFour = year % 4 === 0;
+    // const isCentury = year % 100 === 0;
+    // const isYearDivisibleByFourHundred = year % 400 === 0;
+
+    // if (isYearDivisibleByFour && (!isCentury || isYearDivisibleByFourHundred)){
+    //     console.log('year%4: ', !isYearDivisibleByFour);
+    //     console.log('!year%400: ', !isCentury);
+    //     console.log('year%400: ', !isYearDivisibleByFourHundred);
+    //     return true;
+    // } else {
+    //     return false;
+    // }
 };
+// leapYears(501);
 
 // Do not edit below this line
 module.exports = leapYears;
