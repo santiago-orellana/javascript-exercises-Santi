44414d3e582eda93de5f3731a3a0372bf3f6a5c7
santiago-orellana <135621516+santiago-orellana@users.noreply.github.com>
2025-11-25 13:49:27 +0100

    ex 12 y 13

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	array.forEach(num => {
+    // console.log("num: ", num);
+    sum += num;
+  });
+
+  // console.log('Suma total: ', sum);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiply = 1;
+  array.forEach( num => {
+    multiply *= num;
+  });
+  // console.log('Resultado: ', multiply);
+  return multiply
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  result = 1;
+  for(let i = 0; i < exponent; i++) {
+    result *= base; 
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  result = 1;
+  for(let i = 1; i <= a; i++) {
+    result *= i; 
+  }
+
+  return result;
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // Ponemos en miniscula la cadena toLowerCase() y usamos el metodo
+    // replace() para remover los caracteres no deseados en el (puntos, 
+    // comas, espacios,...)
+    let re = /[\W_]/g;  // RegExp (Expresiones regulares)
+    let lowRegStr = string.toLowerCase().replace(re, '');
 
+    // Utilizamos los metodos de encadenamiento con funcniones integradas
+    // - Convierte el string en array => split('')
+    // - Revierte el array => reverse()
+    // - Convierte el array a string => join('')
+    let reverseStr = lowRegStr.split('').reverse().join('');
+    console.log(reverseStr);
+    return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
